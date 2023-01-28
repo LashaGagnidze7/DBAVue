59085a79ff10a1a9696677fe35d8d8e64796ddff
LashaGagnidze7 <lashadeveloper@gmail.com>
2023-01-28 15:08:59 +0400

    initial commit

---
+++ src/components/AppTemp.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <span>Message: {{ msg }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: 'lasha',
+    };
+  },
+};
+</script>
+
+<style>
+</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import {createApp} from 'vue'
+// import the root component App from a single-file component.
+import AppTemp from './components/AppTemp.vue'
 
-createApp(App).mount('#app')
+const app = createApp(AppTemp)
+app.mount('#app')

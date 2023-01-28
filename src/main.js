279d8af1750ded953dd88891a23774d89b1c53c0
LashaGagnidze7 <lashadeveloper@gmail.com>
2023-01-28 19:35:11 +0400

    Conditional Rendering

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from "vue";
 // import the root component App from a single-file component.
-import App from "./learn/ClassAndStyleBindings.vue";
+import App from "./learn/ConditionalRendering.vue";
 
 const app = createApp(App);
 app.mount("#app");

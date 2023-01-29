f2022721a7788cc1e90451c3f88ac5ad95b66929
LashaGagnidze7 <lashadeveloper@gmail.com>
2023-01-29 15:24:54 +0400

    deleting unnecessary files

--- src/main.js
+++ src/main.js
@@ -1,7 +1,4 @@
 import { createApp } from "vue";
-// import the root component App from a single-file component.
 import App from "./learn/EventHandling.vue";
 
-const app = createApp(App);
-app.mount("#app");
-
+createApp(App).mount("#app");

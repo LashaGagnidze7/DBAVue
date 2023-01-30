fbc1822984213d13bb5c15f188411ddada900f9e
LashaGagnidze7 <lashadeveloper@gmail.com>
2023-01-30 20:05:41 +0400

    get data from API

--- src/App.vue
+++ src/App.vue
@@ -1,76 +1,73 @@
 <template>
-  <Header :header="header"></Header>
-  <AddTask @id-name="(task) => add(task)"></AddTask>
-  <SearchTask :tasks="tasks" ></SearchTask>
-  <ActiveTasks
-    @toCompleted="(id) => activeToCompleted(id)"
-    :tasks="tasks"
-  ></ActiveTasks>
-  <CompletedTasks
-    @toActives="(id) => completedToActives(id)"
-    @delete="(id) => completedToDeleted(id)"
-    :tasks="completed"
-  ></CompletedTasks>
+  <select v-model="limit" @change="getData()">
+    <option v-for="(limit, index) in limits" :key="index" :value="limit">Per Page: {{ limit }}</option>
+  </select>
+  <select v-model="lang" @change="getData()">
+    <option v-for="(lang, index) in languages" :key="index" :value="lang">{{ lang }}</option>
+  </select>
+  <table style="margin: 5px">
+    <thead>
+    <tr>
+      <th>Id</th>
+      <th>Name</th>
+      <th>Image</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr v-for="product in products" :key="product.index">
+      <td>{{ product.id }}</td>
+      <td>{{ product.name }}</td>
+      <td><img style="width: 80px; height: auto;" :src="product.img.cover" alt="cover img"/></td>
+    </tr>
+    </tbody>
+  </table>
+  <div>
+    <a :disabled="!result?.first_page_url" @click.prevent="getData(result.first_page_url)">First Page</a>
+    <button :disabled="!result?.prev_page_url" @click="getData(result.prev_page_url)">Previous</button>
+    <button :disabled="!result?.next_page_url" @click="getData(result.next_page_url)">Next</button>
+    <a :disabled="!result?.last_page_url" @click.prevent="getData(result.last_page_url)">Last Page</a>
+  </div>
 </template>
 
 <script>
-import Header from "./components/AppHeader.vue";
-import AddTask from "./components/AddTask.vue";
-import SearchTask from "./components/SearchTask.vue";
-import ActiveTasks from "./components/ActiveTasks.vue";
-import CompletedTasks from "./components/CompletedTasks.vue";
+import axios from 'axios'
 
 export default {
-  components: {
-    Header,
-    AddTask,
-    SearchTask,
-    ActiveTasks,
-    CompletedTasks,
-  },
   data() {
     return {
-      header: "CMC To Do",
-      tasks: [],
-      completed: [],
-    };
+      limits: [5, 10, 15, 20],
+      limit: 5,
+      languages: ['ge', 'en'],
+      lang: 'ge',
+      url: `http://items.magischer.de/api/products`,
+      products: [],
+      result: null
+    }
   },
   methods: {
-    add(task) {
-      this.tasks.push(task);
-    },
-    activeToCompleted(id) {
-      const task = this.tasks.find((task) => task.id === id);
-      this.tasks.splice(this.tasks.indexOf(task), 1);
-      task.completed = true;
-      this.completed.push(task);
-    },
-    completedToActives(id) {
-      const task = this.completed.find((task) => task.id === id);
-      this.completed.splice(this.completed.indexOf(task), 1);
-      task.completed = false;
-      this.tasks.push(task);
-    },
-    completedToDeleted(id) {
-      this.completed = this.completed.filter((task) => task.id !== id);
+    getData(url = this.url) {
+      axios.get(url, {
+        params: {
+          limit: this.limit,
+          lang: this.lang
+        }
+      }).then((response) => {
+        this.result = response.data
+        this.products = response.data.data
+      })
     },
   },
-};
+  mounted() {
+    this.getData()
+  }
+}
 </script>
 
-<style>
-#app {
-  margin: auto;
-  width: 25%;
-  padding: 10px;
-  border: 1px solid black;
-  border-radius: 10px;
-  font-size: x-large;
+<style scoped>
+* {
+  margin: 3px;
 }
-ul {
-  list-style-type: none;
-}
-li {
-  display: block;
+a, button, select {
+  cursor: pointer;
 }
 </style>

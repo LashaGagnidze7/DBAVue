c47977f9312fe2da28b4534af84d764876f87332
LashaGagnidze7 <lashadeveloper@gmail.com>
2023-02-01 20:16:22 +0400

    vuejs.org/examples

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,9 @@
 <script setup>
-import useProducts from './components/composables/useProducts'
+import { ref } from 'vue'
 
-const { products, getProducts, meta, product, showProduct } = useProducts()
+const message = ref('Hello World!')
 </script>
 
 <template>
-  <table>
-    <thead>
-      <th>Id</th>
-      <th>Name</th>
-      <th>Details</th>
-    </thead>
-    <tbody>
-      <tr v-for="item in products" :key="item.index">
-        <td>{{ item.id }}</td>
-        <td>{{ item.name }}</td>
-        <td><button @click.prevent="showProduct(item.id)">Info</button></td>
-      </tr>
-    </tbody>
-  </table>
-  <div>
-    <button @click.prevent="getProducts(meta.first_page_url)" :disabled="!meta?.first_page_url">first</button>
-    <button @click.prevent="getProducts(meta?.prev_page_url)" :disabled="!meta?.prev_page_url">prev</button>
-    <button @click.prevent="getProducts(meta?.next_page_url)" :disabled="!meta?.next_page_url">next</button>
-    <button @click.prevent="getProducts(meta.last_page_url)" :disabled="!meta?.last_page_url">last</button>
-  </div>
-  <div>
-    {{ product }}
-  </div>
+  <h1>{{ message }}</h1>
 </template>

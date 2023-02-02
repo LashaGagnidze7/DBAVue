ad820c6fa3950218e4f4227d1a8e2051babe17ff
LashaGagnidze7 <lashadeveloper@gmail.com>
2023-02-02 13:20:28 +0400

    without css

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,36 @@
 <script setup>
-import useProducts from './composables/useProducts'
-const { products, getProducts, meta, product, showProduct } = useProducts()
+import axios from "axios";
+import { ref, onMounted } from 'vue'
+
+const apiUrl = 'http://items.magischer.de/api/products'
+const limit = 5
+const lang = 'en'
+const products = ref([])
+const meta = ref(null)
+const product = ref(null)
+
+function getProducts(url = apiUrl) {
+    axios.get(url, {
+        params: {
+            limit: limit,
+            lang: lang
+        }
+    }).then((res) => {
+        meta.value = res.data
+        products.value = res.data.data
+    })
+}
+
+function showProduct(id) {
+    axios.get((apiUrl + '/' + id),).then((res) => {
+        product.value = res.data.data
+    })
+}
+
+onMounted(function () {
+    getProducts(apiUrl)
+
+})
 </script>
 
 <template>

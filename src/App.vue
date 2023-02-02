4b80995328bf1a2e4779f38b942b2d4e4cdf1991
LashaGagnidze7 <lashadeveloper@gmail.com>
2023-02-02 12:36:26 +0400

    Comp API

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,30 @@
 <script setup>
-import { ref } from 'vue'
-import TodoItem from './TodoItem.vue'
-
-const groceryList = ref([
-    { id: 0, text: 'Vegetables' },
-    { id: 1, text: 'Cheese' },
-    { id: 2, text: 'Whatever else humans are supposed to eat' }
-])
+import useProducts from './composables/useProducts'
+const { products, getProducts, meta, product, showProduct } = useProducts()
 </script>
 
 <template>
-    <ol>
-        <TodoItem v-for="item in groceryList" :todo="item" :key="item.id"></TodoItem>
-    </ol>
+  <table>
+    <thead>
+      <th>Id</th>
+      <th>Name</th>
+      <th>Details</th>
+    </thead>
+    <tbody>
+      <tr v-for="item in products" :key="item.index">
+        <td>{{ item.id }}</td>
+        <td>{{ item.name }}</td>
+        <td><button @click.prevent="showProduct(item.id)">Info</button></td>
+      </tr>
+    </tbody>
+  </table>
+  <div>
+    <button @click.prevent="getProducts(meta.first_page_url)" :disabled="!meta?.first_page_url">first</button>
+    <button @click.prevent="getProducts(meta?.prev_page_url)" :disabled="!meta?.prev_page_url">prev</button>
+    <button @click.prevent="getProducts(meta?.next_page_url)" :disabled="!meta?.next_page_url">next</button>
+    <button @click.prevent="getProducts(meta.last_page_url)" :disabled="!meta?.last_page_url">last</button>
+  </div>
+  <div>
+    {{ product }}
+  </div>
 </template>

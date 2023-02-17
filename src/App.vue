701001ddc9bc051b17a4fc6a3590a3593ecdf3c8
LashaGagnidze7 <lashadeveloper@gmail.com>
2023-02-17 13:25:36 +0400

    nabbar style added

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,6 @@ import { useStore } from "vuex";
 import { computed, onMounted } from "vue";
 
 const store = useStore();
-const linkStyle = "text-blue-500 hover:text-blue-800";
 
 const watchlistLen = computed(
   () => store.getters["watchlist/getWatchlistLength"]
@@ -14,14 +13,38 @@ onMounted(() => store.dispatch("api/fetchCurrencies"));
 </script>
 
 <template>
-  <search></search>
-  <div class="flex flex-row space-x-4">
-    <router-link to="/" :class="linkStyle"> Youtube </router-link>
-    <router-link to="/watchlist" :class="linkStyle">
-      Watchlist <sup>{{ watchlistLen }}</sup>
-    </router-link>
-    <router-link to="/add-video" :class="linkStyle"> Add Video </router-link>
-    <router-link to="/api" :class="linkStyle"> Currencies </router-link>
-  </div>
+  <nav
+    class="flex items-center justify-between flex-wrap bg-teal-500 p-6 rounded-sm"
+  >
+    <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+      <search></search>
+      <div class="text-sm lg:flex-grow">
+        <router-link
+          to="/"
+          class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
+        >
+          Youtube
+        </router-link>
+        <router-link
+          to="/watchlist"
+          class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
+        >
+          Watchlist <sup>{{ watchlistLen }}</sup>
+        </router-link>
+        <router-link
+          to="/add-video"
+          class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
+        >
+          Add Video
+        </router-link>
+        <router-link
+          to="/api"
+          class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white"
+        >
+          Currencies
+        </router-link>
+      </div>
+    </div>
+  </nav>
   <router-view />
 </template>
